618fb557808008d3b4504ef5e7ac8ea6ac076c7b
lafang <raksasparkle@gmail.com>
2024-06-11 14:03:17 +0700

    hotfix : quick fix on released version 1.1, fix inconsistent variable name in newFeatured.js

--- src/pages/newfeatured/newFeatured.js
+++ src/pages/newfeatured/newFeatured.js
@@ -337,11 +337,11 @@ class NewFeaturedCategories extends HTMLElement{
         sideDropdown("bestBtn", "bestContent", "h-[120px]")
         // Function to implement dropdown functionality
         function sideDropdown(btn,content,h){
-        const Btn = document.querySelector(`#${btn}`);
-        const Content = document.querySelector(`#${content}`);
+        const btn = document.querySelector(`#${btn}`);
+        const content = document.querySelector(`#${content}`);
         
-        Btn.addEventListener("click",function(){
-            Content.classList.toggle(`${h}`)
+        btn.addEventListener("click",function(){
+            content.classList.toggle(`${h}`)
         })
         }
 
